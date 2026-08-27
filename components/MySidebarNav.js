18e245c41cbf5122c17308aad6ec42b9a30adee2
simminhyoung <jhon54378@gmail.com>
2026-08-27 22:49:35 +0900

    Add files via upload

--- components/MySidebarNav.js
+++ components/MySidebarNav.js
@@ -6,7 +6,7 @@ import { usePathname } from "next/navigation";
 const ITEMS = [
   { href: "/my", label: "홈", hint: "내가 기록한 독후감" },
   { href: "/my/stats", label: "통계", hint: "읽은 흐름과 취향" },
-  { href: "/my/past", label: "과거의 내가 보내는 말", hint: "지난 기록에서 온 문장" },
+  { href: "/my/past", label: "과거의 말", hint: "지난 기록에서 온 문장" },
 ];
 
 export default function MySidebarNav() {
